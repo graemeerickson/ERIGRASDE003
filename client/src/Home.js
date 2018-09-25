92e4fef357e98cb4b7a5572e70d330330a387c5a
graemeerickson <graemeerickson@gmail.com>
2018-09-24 21:56:09 -0700

    Populated with call-to-action text and an image pointing toward navbar links

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import pointerImage from './img/pointing-up.png';
 
 class Home extends Component {
   render() {
     return (
-      <div>
-        <h1>Home component</h1>
-      </div>
+      <main>
+        <section id="home-section">
+          <img id="pointer-up-image" src={pointerImage} alt="" /><br />
+          <h3 className="home-call-to-action">To read the latest from Expedia on Facebook or to read Mark Twain's most popular quotes, explore the links above</h3>
+        </section>
+      </main>
     );
   }
 }
