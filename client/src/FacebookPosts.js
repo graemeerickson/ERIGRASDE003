d5a65e23dce6aa05679d88260a5aa954617a3b2b
graemeerickson <graemeerickson@gmail.com>
2018-09-24 23:10:45 -0700

    Receive FB posts data from backend and loop through to render on screen. Display loading message until data is received from backend and state is updated accordingly.

--- client/src/FacebookPosts.js
+++ client/src/FacebookPosts.js
@@ -1,22 +1,51 @@
 import React, { Component } from 'react';
 
 class FacebookPosts extends Component {
-  
+  constructor() {
+    super();
+    this.state = {
+      loaded: false,
+      posts: []
+    }
+  }
+
   componentDidMount = () => {
     fetch('http://localhost:3001/facebook')
       .then(response => response.json())
       .then(posts => {
-        console.log('posts received from backend:', posts)
+        this.setState({
+          loaded: true,
+          posts
+        })
       })
       .catch(err => console.log('Error while parsing:', err))
   }
 
   render() {
-    return (
-      <div>
-        <h1>FacebookPosts component</h1>
-      </div>
-    )
+    
+    const posts = this.state.posts.map((post, index) => {
+      return (
+        <div key={index}>
+          <h3>{post.title}</h3>
+          <p>{post.text}</p>
+          <small>{post.timestamp}</small>
+        </div>
+      )
+    })
+
+    if (this.state.loaded) {
+      return (
+        <main>
+          {posts}
+        </main>
+      )
+    } else {
+      return (
+        <main>
+          <h2>Fetching Expedia Facebook posts...</h2>
+        </main>
+      )
+    }
   }
 }
 
