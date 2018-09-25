de618b3a0edc0a29006926ba6c7737d222667299
graemeerickson <graemeerickson@gmail.com>
2018-09-24 22:23:38 -0700

    Implemented fetch call to backend /facebook route to test end-to-end connectivity

--- client/src/FacebookPosts.js
+++ client/src/FacebookPosts.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 
 class FacebookPosts extends Component {
+  
+  componentDidMount = () => {
+    fetch('http://localhost:3001/facebook')
+      .then(response => response.json())
+      .then(posts => {
+        console.log('posts received from backend:', posts)
+      })
+      .catch(err => console.log('Error while parsing:', err))
+  }
+
   render() {
     return (
       <div>
