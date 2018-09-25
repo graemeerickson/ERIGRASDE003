702310793f489c310107de5eb4406746c22d3b9b
graemeerickson <graemeerickson@gmail.com>
2018-09-24 23:00:14 -0700

    Implemented functionality to execute Python Facebook-scraping script, then read the data from the outputted text file into a local data structure. Successfully sending data back to the frontend.

--- server/routes/facebook.js
+++ server/routes/facebook.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const fs = require('fs');
+const {PythonShell} = require('python-shell');
+// const pythonScript = require('expedia_fb_posts.py');
 const router = express.Router();
 
+const fbPostsFile = './scripts/expedia_fb_posts.txt';
+const pythonScript = './scripts/expedia_fb_posts.py';
+
 router.get('/', (req, res) => {
-  console.log('reached /facebook route');
+  // Execute facebook posts Python script, then read the resulting file into a local data structure.
+  // Send data back to frontend to display on the UI.
+  PythonShell.run(pythonScript, null, (err) => {
+    if (err) console.log('error:', err);
+    const postsArr = fs.readFileSync(fbPostsFile, 'utf8');
+    res.send(JSON.parse(postsArr));
+  })
 })
 
 module.exports = router;
