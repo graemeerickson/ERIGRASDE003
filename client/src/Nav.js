b643c48e225af6a2f2157a4a9f1166d886506a49
graemeerickson <graemeerickson@gmail.com>
2018-09-24 22:11:31 -0700

    Replaced 'Facebook' in navbar with Facebook logo

--- client/src/Nav.js
+++ client/src/Nav.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import fbLogo from './img/flogo_RGB_HEX-72.svg';
 
 class Nav extends Component {
   render() {
@@ -7,7 +8,7 @@ class Nav extends Component {
       <span className="nav-link">
         <ul className="nav-list">
           <li className="nav-item"><NavLink exact to="/" activeClassName="active">Home</NavLink></li>
-          <li className="nav-item"><NavLink to="/facebook" activeClassName="active">Expedia on Facebook</NavLink></li>
+          <li className="nav-item"><NavLink to="/facebook" activeClassName="active">Expedia on &nbsp;<img className="fb-logo" src={fbLogo} alt="facebook-logo" /></NavLink></li>
           <li className="nav-item"><NavLink to="/quotes" activeClassName="active">Quotes</NavLink></li>
         </ul>
       </span>
