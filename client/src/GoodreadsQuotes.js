3a7b630a7edfa2f7ee18e4f5ddd5bc61649af105
graemeerickson <graemeerickson@gmail.com>
2018-09-25 10:28:25 -0700

    Render quotes on the UI. Added state to show progress indicator until quotes data is received.

--- client/src/GoodreadsQuotes.js
+++ client/src/GoodreadsQuotes.js
@@ -1,22 +1,52 @@
 import React, { Component } from 'react';
 
 class GoodreadsQuotes extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loaded: false,
+      quotesArr: []
+    }
+  }
 
   componentDidMount = () => {
     fetch('http://localhost:3001/quotes')
       .then(response => response.json())
-      .then(quotes => {
-        console.log('quotes received from backend:', quotes);
+      .then(quotesArr => {
+        this.setState({
+          loaded: true,
+          quotesArr
+        })
       })
       .catch(err => console.log('An error occured while parsing!', err))
   }
 
   render() {
-    return (
-      <div>
-        <h1>GoodreadsQuotes component</h1>
-      </div>
-    )
+    const quotes = this.state.quotesArr.map((quote, index) => {
+      return (
+        <div className="post-card quote-post" key={index}>
+          <p>{quote.quoteText}</p>
+          <small>Tags: {quote.quoteTags.join()}</small>
+          <br />
+          <small>{quote.quoteLikes}</small>
+        </div>
+      )
+    })
+
+    if (this.state.loaded) {
+      return (
+        <main>
+          {quotes}
+        </main>
+      )
+    } else {
+      return (
+        <main>
+          <h3>Fetching Mark Twain quotes...</h3>
+          <div className="spinner"></div>
+        </main>
+      )
+    }
   }
 }
 
