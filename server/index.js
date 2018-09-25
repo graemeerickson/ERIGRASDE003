1255e6b4f9b92f91b84e3e373bbda7e795ee46f5
graemeerickson <graemeerickson@gmail.com>
2018-09-24 22:27:37 -0700

    Built out /quotes route

--- server/index.js
+++ server/index.js
@@ -2,5 +2,6 @@ const express = require('express');
 const app = express();
 
 app.use('/facebook', require('./routes/facebook'));
+app.use('/quotes', require('./routes/quotes'));
 
 app.listen(process.env.PORT || 3001);
