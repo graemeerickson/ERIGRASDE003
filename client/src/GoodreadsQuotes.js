072ca541753006caccc4c571d483d4e0a807f43c
graemeerickson <graemeerickson@gmail.com>
2018-09-24 22:28:21 -0700

    Implemented fetch call to backend /quotes route to test end-to-end connectivity

--- client/src/GoodreadsQuotes.js
+++ client/src/GoodreadsQuotes.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 
 class GoodreadsQuotes extends Component {
+
+  componentDidMount = () => {
+    fetch('http://localhost:3001/quotes')
+      .then(response => response.json())
+      .then(quotes => {
+        console.log('quotes received from backend:', quotes);
+      })
+      .catch(err => console.log('An error occured while parsing!', err))
+  }
+
   render() {
     return (
       <div>
