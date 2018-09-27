442a0db5d0d17d1db7324b0b39e8629d0d96fbf5
graemeerickson <graemeerickson@gmail.com>
2018-09-26 22:57:23 -0700

    Added route to /quotes/:id

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import FacebookPosts from './FacebookPosts';
-import Home from './Home';
 import GoodreadsQuotes from './GoodreadsQuotes';
+import Home from './Home';
 import Nav from './Nav';
 import './App.css';
 
@@ -16,6 +16,7 @@ class App extends Component {
             <Route exact path="/" component={Home} />
             <Route exact path="/facebook" component={FacebookPosts} />
             <Route exact path="/quotes" component={GoodreadsQuotes} />
+            <Route exact path="/quotes/:id" component={GoodreadsQuotes} />
           </div>
         </Router>
       </div>
