dd94a11eee8aeafd7e5582c5df3dd84b6384f688
graemeerickson <graemeerickson@gmail.com>
2018-09-26 22:57:46 -0700

    Implemented logic to show button if user isn't authenticated, else fetch and show quotes

--- client/src/GoodreadsQuotes.js
+++ client/src/GoodreadsQuotes.js
@@ -1,25 +1,44 @@
 import React, { Component } from 'react';
+
 const SERVER_URL = 'http://localhost:3001';
+const AUTH_PATH = '/auth';
+const QUOTES_PATH = '/quotes';
 
 class GoodreadsQuotes extends Component {
   constructor() {
     super();
     this.state = {
       loaded: false,
+      authorized: false,
       quotesArr: []
     }
   }
 
   componentDidMount = () => {
-    fetch('http://localhost:3001/quotes')
-      .then(response => response.json())
-      .then(quotesArr => {
-        this.setState({
-          loaded: true,
-          quotesArr
+    const authorizedInLocalStorage = localStorage.getItem('authorized_with_goodreads');
+    const urlParam = this.props.match.params.id;
+    // if '1' is in the url string, then assume the user is authorized by Goodreads. if '1' isn't
+    // in the url string, check if we've written to localStorage that this user is authorized.
+    if (urlParam === '1' || authorizedInLocalStorage) {
+      fetch(SERVER_URL + QUOTES_PATH)
+        .then(response => response.json())
+        .then(quotesArr => {
+          // write to localstorage
+          localStorage.setItem('authorized_with_goodreads', true);
+          this.setState({
+            loaded: true,
+            authorized: true,
+            quotesArr
+          })
         })
+        .catch(err => console.log('Error occured while fetching data', err))
+    }
+    // not authorized by Goodreads - do not fetch quotes
+    else {
+      this.setState({
+        loaded: true
       })
-      .catch(err => console.log('An error occured while parsing!', err))
+    }
   }
 
   render() {
@@ -34,19 +53,24 @@ class GoodreadsQuotes extends Component {
       )
     })
 
-    if (this.state.loaded) {
+    if (this.state.loaded && this.state.authorized) {
       return (
         <main>
-          <a href={SERVER_URL + '/auth'}>
+          {quotes}
+        </main>
+      )
+    } else if (this.state.loaded && !this.state.authorized) {
+      return (
+        <main>
+          <a href={SERVER_URL + AUTH_PATH}>
             <button className="login-btn">Log into Goodreads to read<br /> Mark Twain's top quotes</button>
           </a>
-          {quotes}
         </main>
       )
     } else {
       return (
         <main>
-          <h3>Fetching Mark Twain quotes...</h3>
+          <h2>Fetching Mark Twain quotes...</h2>
           <div className="spinner"></div>
         </main>
       )
