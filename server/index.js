1edf71bacc31dca0f6ccf4abb8db1da61eeadb69
graemeerickson <graemeerickson@gmail.com>
2018-09-26 20:41:16 -0700

    Hooked up initial auth stub route

--- server/index.js
+++ server/index.js
@@ -6,5 +6,6 @@ app.use(cors())
 
 app.use('/facebook', require('./routes/facebook'));
 app.use('/quotes', require('./routes/quotes'));
+app.use('/auth', require('./routes/auth'));
 
 app.listen(process.env.PORT || 3001);
