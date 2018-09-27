4d5efb796c553f776ba9605ef89f2fa540a479b2
graemeerickson <graemeerickson@gmail.com>
2018-09-26 22:56:53 -0700

    Updated copy

--- client/src/Home.js
+++ client/src/Home.js
@@ -7,7 +7,7 @@ class Home extends Component {
       <main>
         <section id="home-section">
           <img id="pointer-up-image" src={pointerImage} alt="" /><br />
-          <h3 className="home-call-to-action">To read the latest from Expedia on Facebook or to read Mark Twain's most popular quotes, explore the links above</h3>
+          <h3 className="home-call-to-action">To read the latest from Expedia on Facebook or to read Mark Twain's most popular quotes, explore the links above.</h3>
         </section>
       </main>
     );
